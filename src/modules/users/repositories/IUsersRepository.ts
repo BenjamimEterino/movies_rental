e423124ada9d07cddcb90c426c1ab310c5bd31a7
BenjamimEterino <benjamimtoretto901@gmail.com>
2023-03-08 17:02:21 +0200

    Restart project

--- src/modules/users/repositories/IUsersRepository.ts
+++
@@ -1,9 +0,0 @@
-import { ICreateUsersDTO } from "../dtos/ICreateUsersDTO"
-import {user} from ".prisma/client"
-
-interface IUsersRepository {
-create({name, email, password}: ICreateUsersDTO): Promise<user>
-findByEmail(email: string): Promise<user>
-}
-
-export {IUsersRepository}
