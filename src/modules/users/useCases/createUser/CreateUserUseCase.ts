0eaf80e176a37fdfb7d8a8973dfe70595ce19f20
BenjamimEterino <benjamimtoretto901@gmail.com>
2023-03-13 19:36:48 +0200

    User defined errors

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,28 +2,29 @@ import { inject, injectable } from "tsyringe";
 import { IcreateUsersDTO } from "../../dtos/ICreateUsersDTO";
 import { IUsersRepo } from "../../repositories/IUsersRepo";
 import { hash } from "bcrypt";
+import { AppError } from "../../../../errors/AppError";
 
 @injectable()
 class CreateUserUseCase {
   constructor(
     @inject("UsersRepo")
     private userRepo: IUsersRepo
-    ) {}
+  ) {}
 
   async execute({ name, email, password }: IcreateUsersDTO) {
     const userExists = await this.userRepo.findByEmail(email);
 
     if (userExists) {
-      throw new Error("User existe");
+      throw new AppError("Users Exists!");
     }
 
     const passwordHash = await hash(password, 8);
 
     const user = await this.userRepo.create({
-        email,
-        name,
-        password: passwordHash
-    })
+      email,
+      name,
+      password: passwordHash,
+    });
 
     return user;
   }
